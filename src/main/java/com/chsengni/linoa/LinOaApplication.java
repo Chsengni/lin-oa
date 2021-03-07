c74e0744b0062e04eee5c2b3778d2ab54ba1ac05
Chsengni <2371778707@qq.com>
2021-03-07 12:44:11 +0800

    Initial commit

---
+++ src/main/java/com/chsengni/linoa/LinOaApplication.java
@@ -0,0 +1,13 @@
+package com.chsengni.linoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinOaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinOaApplication.class, args);
+    }
+
+}
